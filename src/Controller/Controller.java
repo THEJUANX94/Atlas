c9b1d4d1151f5a34a476d22ce5d25788b68312d1
THEJUANX94 <sebastianmn03@hotmail.com>
2023-07-06 14:37:51 -0500

    w

---
+++ src/Controller/Controller.java
@@ -0,0 +1,14 @@
+package Controller;
+
+public class Controller {
+
+    public void run(){
+
+    }
+    
+    public static void main(String[] args) {
+        Controller controller = new Controller();
+        controller.run();
+    }
+    
+}
